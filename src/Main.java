043ccfd2e17322df6d120f2c4e8faf0149fe77e8
Fiser Tomas <fiser.tomas@feg.eu>
2025-04-23 23:48:51 +0200

    New class Seller, creating seller objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Seller seller1 = new Seller(LocalDate.of(1995, 2, 15),"Neo Anderson", 475, 65.3, false);
+        Seller seller2 = new Seller(LocalDate.of(1987, 9, 23),"Agent Smith", 666, 121.5, true);
+
+        System.out.println("First sellers name is "+ seller1.getName() + ".");
+        System.out.println("Second sellers name is "+ seller2.getName() + ".");
+
     }
 }
--- src/Seller.java
+++ src/Seller.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class Seller {
+    private String name;
+    private LocalDate birthDate;
+    private int numberOfTransactions;
+    private double tonsOfCarrotSold;
+    private boolean isSeniorSeller;
+
+    public Seller(LocalDate birthDate, String name, int numberOfTransactions, double tonsOfCarrotSold, boolean isSeniorSeller) {
+        this.birthDate = birthDate;
+        this.name = name;
+        this.numberOfTransactions = numberOfTransactions;
+        this.tonsOfCarrotSold = tonsOfCarrotSold;
+        this.isSeniorSeller = isSeniorSeller;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getNumberOfTransactions() {
+        return numberOfTransactions;
+    }
+
+    public void setNumberOfTransactions(int numberOfTransactions) {
+        this.numberOfTransactions = numberOfTransactions;
+    }
+
+    public double getTonsOfCarrotSold() {
+        return tonsOfCarrotSold;
+    }
+
+    public void setTonsOfCarrotSold(double tonsOfCarrotSold) {
+        this.tonsOfCarrotSold = tonsOfCarrotSold;
+    }
+
+    public boolean isSeniorSeller() {
+        return isSeniorSeller;
+    }
+
+    public void setSeniorSeller(boolean seniorSeller) {
+        isSeniorSeller = seniorSeller;
+    }
+
 }
